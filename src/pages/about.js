64adc01a81de1a3c6a4cd80568cf4bde3fd0d17f
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-12 18:34:21 +0200

    simple routing

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return <Layout>hello from about page</Layout>
+}
+
+export default About
---
+++ src/pages/tags.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Tags = () => {
+  return <Layout>hello from tags page</Layout>
+}
+
+export default Tags
