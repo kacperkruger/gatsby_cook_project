48ff19aa218dbdbb84dd9b0366f498efcb635653
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-24 15:55:27 +0200

    impl of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "../assets/images/logo.svg"
 import { Link } from "gatsby"
+import { FaAlignJustify } from "react-icons/all"
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
     <nav className={"navbar"}>
-      <div className="nav-center">
-        <div className="nav-header">
+      <div className={"nav-center"}>
+        <div className={"nav-header"}>
           <Link to={"/"}>
             <img src={logo} alt={"SimpleRecipes"} />
           </Link>
-          <div className="nav-links show-links"></div>
+          <button className={"nav-btn"} onClick={() => setShow(!show)}>
+            <FaAlignJustify />
+          </button>
+        </div>
+        <div className={show ? "nav-links show-links" : "nav-links"}>
+          <Link to={"/"} className={"nav-link"} activeClassName={"active-link"}>
+            home
+          </Link>
+          <Link
+            to={"/recipes"}
+            className={"nav-link"}
+            activeClassName={"active-link"}
+          >
+            recipes
+          </Link>
+          <Link
+            to={"/tags"}
+            className={"nav-link"}
+            activeClassName={"active-link"}
+          >
+            tags
+          </Link>
+          <Link
+            to={"/about"}
+            className={"nav-link"}
+            activeClassName={"active-link"}
+          >
+            about
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to={"/contact"} className={"btn"}>
+              contact
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
