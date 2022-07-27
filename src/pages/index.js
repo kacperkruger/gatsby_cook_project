8aa8d2d1a9473bdaeb24629beed319c24909b692
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-27 20:10:25 +0200

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,29 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Home = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <main className={"page"}>
+        <header className={"hero"}>
+          <StaticImage
+            src={"../assets/images/main.jpeg"}
+            alt={"eggs"}
+            className={"hero-img"}
+            placeholder={"blurred"}
+            layout={"fullWidth"}
+          />
+          <div className={"hero-container"}>
+            <div className={"hero-text"}>
+              <h1>simple recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
+  )
 }
 
 export default Home
