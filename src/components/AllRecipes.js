931b1cc392c4c70fd866663b653e0d3bd3a3f0ec
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-31 15:15:11 +0200

    imports

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,9 +1,13 @@
 import React from "react"
+import TagList from "./TagList"
+import RecipeList from "./RecipeList"
 
 const AllRecipes = () => {
   return (
     <div>
       <h4>All recipes</h4>
+      <TagList />
+      <RecipeList />
     </div>
   )
 }
