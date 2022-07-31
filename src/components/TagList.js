c26dce5330ced4d73ae71f42926a477442360868
Kacper Kruger <kacper.kruger1@gmail.com>
2022-08-01 01:38:26 +0200

    tag list component

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,7 +1,24 @@
 import React from "react"
+import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
-const TagList = () => {
-  return <div>tags</div>
+const TagList = ({ recipes }) => {
+  const tags = setupTags(recipes)
+  return (
+    <div className={"tags-container"}>
+      <h4>recipes</h4>
+      <div className={"tags-list"}>
+        {tags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagList
