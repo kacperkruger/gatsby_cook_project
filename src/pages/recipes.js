53e7493ac4dcae074b856aacd3a2126ba02591ef
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-31 15:18:31 +0200

    importing all recipes

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,8 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout"
+import AllRecipes from "../components/AllRecipes"
 
 const Recipes = () => {
-  return <Layout>hello from recipes page</Layout>
+  return (
+    <Layout>
+      <main className="page">
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
 }
 
 export default Recipes
