46235fa582cd37b90c7efb489396964050df1fa3
Kacper Kruger <kacper.kruger1@gmail.com>
2022-07-31 15:35:51 +0200

    query for recipes

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,14 +1,36 @@
 import React from "react"
 import TagList from "./TagList"
 import RecipeList from "./RecipeList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+        title
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 const AllRecipes = () => {
+  const {
+    allContentfulRecipe: { nodes: recipes },
+  } = useStaticQuery(query)
   return (
-    <div>
-      <h4>All recipes</h4>
-      <TagList />
-      <RecipeList />
-    </div>
+    <section className={"recipes-container"}>
+      <TagList recipes={recipes} />
+      <RecipeList recipes={recipes} />
+    </section>
   )
 }
 
